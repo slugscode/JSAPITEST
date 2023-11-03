a9c00d506243fecd93320fce98b3d2a3485e6ac5
anisyakh <anisyakharisman@gmail.com>
2023-11-03 15:00:33 +0700

    test api using javascript

--- test/comments.js
+++ test/comments.js
@@ -5,13 +5,13 @@ const expect = require("chai").expect;
 
 // checking response from
 describe("Comments API Tests", () => {
-  //it("should return all the comments using GET /comments", () => {
-   // return request.get("comments").then((res) => {
-    //  expect(res.statusCode).to.be.eql(200);
-     // expect(res.body.data).not.to.be.empty;
-     // expect(res.body.data.length).to.be.eql(10);
-   // });
- // });
+  it("should return all the comments using GET /comments", () => {
+    return request.get("comments").then((res) => {
+      expect(res.statusCode).to.be.eql(200);
+      expect(res.body.data).not.to.be.empty;
+      expect(res.body.data.length).to.be.eql(10);
+    });
+  });
 
   it("should return a specific comment using GET /comments/{comment_id}", () => {
     // Replace {comment_id} with the actual comment ID you want to test
